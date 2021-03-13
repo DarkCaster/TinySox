ee1e5f3dddcad3c15a6b8dd80ebdfeb9df852f4a
DarkCaster <dark.caster@outlook.com>
2021-03-13 15:30:34 +0300

    JobFactory: add some optimizations for std::vector

--- Src/JobFactory.cpp
+++ Src/JobFactory.cpp
@@ -23,23 +23,22 @@ std::vector<std::shared_ptr<IJob>> JobFactory::CreateJobsFromResult(const IJobRe
         for(auto &cState:result.state.handlerClaimStates)
             if(cState.counter<1)
                 commService.DisposeHandler(cState.handlerID);
-        return std::vector<std::shared_ptr<IJob>>();
+        return std::vector<std::shared_ptr<IJob>>(0);
     }
 
     //new client connected, next job will do all the initial handshake routines needed for socks protocol
     if(source.resultType==JR_NEW_CLIENT)
     {
         auto result=static_cast<const INewClientJobResult&>(source);
-        return std::vector<std::shared_ptr<IJob>>{ std::make_shared<Job_ClientHandshake>(commService,commManager,config,State().AddHandler(result.handlerID)) };
+        return std::vector<std::shared_ptr<IJob>>(1,std::make_shared<Job_ClientHandshake>(commService,commManager,config,State().AddHandler(result.handlerID)));
     }
 
     //handhake and CONNECT socks command complete, next jobs will transfer data across newly created TCP tunnel
     if(source.resultType==JR_CLIENT_MODE_CONNECT)
     {
         auto result=static_cast<const IModeConnectJobResult&>(source);
-        std::vector<std::shared_ptr<IJob>> jobs;
-        jobs.push_back(std::make_shared<Job_TCPTunnel>(commManager,result.state,config,true));
-        jobs.push_back(std::make_shared<Job_TCPTunnel>(commManager,result.state,config,false));
+        std::vector<std::shared_ptr<IJob>> jobs({std::make_shared<Job_TCPTunnel>(commManager,result.state,config,true),
+         std::make_shared<Job_TCPTunnel>(commManager,result.state,config,false)});
         return jobs;
     }
 
